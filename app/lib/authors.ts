91b1c2ff00cd66ce37d9ff99be70336ce072d524
tomokisun <github@tomokisun.com>
2025-04-22 14:55:39 +0900

    feat: anonymous

--- app/lib/authors.ts
+++ app/lib/authors.ts
@@ -19,7 +19,7 @@ export const getAuthor = (authorId?: string): Author => {
   if (!authorId || !authors[authorId]) {
     // Default author if not specified or not found
     return {
-      name: 'ONE Engineering',
+      name: 'Anonymous',
       icon: 'https://pbs.twimg.com/profile_images/1660378428145807360/6tw_dYvx_400x400.png',
     };
   }
